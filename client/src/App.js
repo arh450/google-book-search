70ebe56b107db1b2f6ad9afeed7d7a996d1e6e20
Gus Heptig <gusheptig@gmail.com>
2020-09-23 12:03:25 -0400

    created nav and routed pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 import "./App.css";
 
-function App() {
-  return <div id="app">google books search</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" render={(props) => <Search {...props} />} />
+          <Route exact path="/saved" render={(props) => <Saved {...props} />} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
-
-export default App;
