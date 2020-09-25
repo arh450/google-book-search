e784f89830cc3bcabdc3e1c80f12ad57e85d63a2
Gus Heptig <gusheptig@gmail.com>
2020-09-24 22:26:30 -0400

    added utils folder for api

---
+++ client/src/utils/API.js
@@ -0,0 +1,19 @@
+import Axios from "axios";
+
+export default {
+  getBook: function (query) {
+    return;
+  },
+
+  saveBook: function (bookData) {
+    return;
+  },
+
+  savedBooks: function () {
+    return;
+  },
+
+  deleteBook: function (id) {
+    return;
+  },
+};
