1ee01cc8d42a1dd54a8024352b443496ad676c3d
Gus Heptig <gusheptig@gmail.com>
2020-09-24 21:55:22 -0400

    created basic results component for books

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "../components/Header/Header.js";
+import Results from "../components/Results/Results.js";
 
 export default class Saved extends Component {
   render() {
     return (
       <>
         <Header />
+        <Results />
       </>
     );
   }
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../components/Header/Header.js";
 import Search from "../components/Search/Search.js";
+import Results from "../components/Results/Results.js";
 
 export default class index extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class index extends Component {
       <>
         <Header />
         <Search />
+        <Results />
       </>
     );
   }
